40986b8b80e37ed5003619d2aa36f9556207fd6d
Icaro Seidi Asato <icaroseidi@hotmail.com>
2023-05-03 16:54:41 -0300

    adicionado teste de transferência de servidor

--- git_teste.java
+++ git_teste.java
@@ -1,5 +1,6 @@
 public class git_teste {
     public static void main(String[] args) {
         System.out.println("teste de git");
+        System.out.println("teste de transferência para servidor");
     }
 }
