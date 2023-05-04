8b6d613e7a73901dc3d8c29abe1d1abe99018011
Icaro Seidi Asato <icaroseidi@hotmail.com>
2023-05-04 17:11:16 -0300

    adicionado "de git"

--- git_teste.java
+++ git_teste.java
@@ -1,6 +1,6 @@
 public class git_teste {
     public static void main(String[] args) {
         System.out.println("teste de git");
-        System.out.println("teste de transferência para servidor");
+        System.out.println("teste de transferência para servidor de git");
     }
 }
